e74596520f4d237921cc3078080432de7b5f779a
Grigory Starinkin <starinkin@gmail.com>
2019-01-28 17:14:46 +0000

    return decoded picture properties along with the body

--- c_src/Tensorflex.c
+++ c_src/Tensorflex.c
@@ -1152,6 +1152,25 @@ static ERL_NIF_TERM tensor_to_matrix(ErlNifEnv *env, int argc,
   return ret;
 }
 
+ERL_NIF_TERM decode_properties(ErlNifEnv *env, int width, int height,
+                               int channels) {
+  ERL_NIF_TERM width_term;
+  width_term = enif_make_tuple2(env, enif_make_atom(env, "width"),
+                                enif_make_int(env, width));
+
+  ERL_NIF_TERM height_term;
+  height_term = enif_make_tuple2(env, enif_make_atom(env, "height"),
+                                 enif_make_int(env, height));
+
+  ERL_NIF_TERM channels_term;
+  channels_term = enif_make_tuple2(env, enif_make_atom(env, "channels"),
+                                   enif_make_int(env, channels));
+
+  ERL_NIF_TERM items[3] = {width_term, height_term, channels_term};
+  return enif_make_list_from_array(
+    env, items, sizeof(items) / sizeof(ERL_NIF_TERM));
+}
+
 static ERL_NIF_TERM decode_jpeg(ErlNifEnv *env, int argc,
                                 const ERL_NIF_TERM argv[]) {
   ERL_NIF_TERM jpeg_binary_term;
@@ -1207,8 +1226,11 @@ static ERL_NIF_TERM decode_jpeg(ErlNifEnv *env, int argc,
 
   ERL_NIF_TERM bmp_term;
   bmp_term = enif_make_binary(env, &bmp_binary);
+  ERL_NIF_TERM properties_term;
+  properties_term = decode_properties(env, width, height, num_pixels);
 
-  return enif_make_tuple2(env, enif_make_atom(env, "ok"), bmp_term);
+  return enif_make_tuple3(
+    env, enif_make_atom(env, "ok"), bmp_term, properties_term);
 }
 
 static ErlNifFunc nif_funcs[] = {
